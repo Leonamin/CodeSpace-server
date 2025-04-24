82c2a940ddd4495bedfe906234c8c13d935bc0ee
Leonamin <yism810@gmail.com>
2025-04-24 14:57:13 +0900

    # INIT

---
+++ src/test/java/com/cuteshrew/codespace/codespace/CodeSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuteshrew.codespace.codespace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeSpaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
