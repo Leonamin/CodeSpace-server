c56035fe9cde689c03d8971abbdc84d610b844bd
Leonamin <yism810@gmail.com>
2025-04-28 23:54:16 +0900

    # ADD - CodeSpaceController 코드 스페이스 컨트롤러 생성

---
+++ src/main/java/com/cuteshrew/codespace/codespace/controller/CodeSpaceController.java
@@ -0,0 +1,44 @@
+package com.cuteshrew.codespace.codespace.controller;
+
+import com.cuteshrew.codespace.codespace.dto.codespace.CodeSpaceCreateReq;
+import com.cuteshrew.codespace.codespace.dto.codespace.CodeSpaceDeleteReq;
+import com.cuteshrew.codespace.codespace.dto.codespace.CodeSpaceSummaryRes;
+import com.cuteshrew.codespace.codespace.dto.codespace.CodeSpaceUpdateReq;
+import com.cuteshrew.codespace.codespace.service.CodeSpaceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/codespaces")
+@RequiredArgsConstructor
+public class CodeSpaceController {
+    private final CodeSpaceService codeSpaceService;
+
+    @PostMapping
+    public void createCodeSpace(@RequestBody CodeSpaceCreateReq req) {
+        codeSpaceService.createCodeSpace(req);
+    }
+
+    @PutMapping
+    public void updateCodeSpace(@RequestBody CodeSpaceUpdateReq req) {
+        codeSpaceService.updateCodeSpace(req);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCodeSpace(@PathVariable Long id, @RequestBody CodeSpaceDeleteReq req) {
+        codeSpaceService.deleteCodeSpace(id, req.getPassword());
+    }
+
+    @GetMapping("/{id}")
+    public CodeSpaceSummaryRes getCodeSpaceById(@PathVariable Long id) {
+        return codeSpaceService.getCodeSpaceById(id);
+    }
+
+    @GetMapping
+    public Page<CodeSpaceSummaryRes> getCodeSpaceList(Pageable pageable) {
+        return codeSpaceService.getAllCodeSpaces(pageable);
+    }
+}
